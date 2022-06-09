e6b1a0916e18e97c105e77a66ced2f5396dd8375
Maciej Katafiasz <mathrick@gmail.com>
2022-06-08 20:12:56 -0700

    Add +always-show-recent to magit module

--- modules/vcs/magit/manifest.el
+++ modules/vcs/magit/manifest.el
@@ -1,2 +1,2 @@
-(defflag -forge "Do not enable Magit's forge functionality,
-ie. support for Github, Gitlab, etc.")
+(defflag -forge "Do not enable Magit's forge functionality,ie. support for Github, Gitlab, etc.")
+(defflag +always-show-recent "Always show recent commits. By default, Magit only shows recent if there are no unpushed ones")
--- modules/vcs/magit/packages.el
+++ modules/vcs/magit/packages.el
@@ -1,7 +1,20 @@
 ;; -*- lexical-binding: t; -*-
 
-(use-package magit
-  :defer t)
+(let ((always-show-recent (featurep! +always-show-recent)))
+  (use-package magit
+    :defer t
+    :config
+    (when always-show-recent
+      ;; Instead of showing *either* unpushed *or* recent, always show both
+      (magit-add-section-hook 'magit-status-sections-hook
+                              #'magit-insert-unpushed-to-upstream
+                              #'magit-insert-unpushed-to-upstream-or-recent
+                              'replace)
+      (magit-add-section-hook 'magit-status-sections-hook
+                              #'magit-insert-recent-commits
+                              ;; Don't move it if it's already included (depends on magit version)
+                              #'magit-insert-unpushed-to-upstream
+                              t))))
 
 (unless (featurep! -forge)
   (use-package forge
