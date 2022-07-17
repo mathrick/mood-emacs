cb19a59ef17d000061c70e4a19d2249f518c52ef
Maciej Katafiasz <mathrick@gmail.com>
2022-07-17 13:27:07 -0700

    Use advice instead of wrapper function for visual-regexp
    This improves compatibility with third-party packages which expect to
    see native isearch functions

--- modules/editing/visual-regexp/packages.el
+++ modules/editing/visual-regexp/packages.el
@@ -16,35 +16,18 @@
     (setq! vr/engine 'pcre2el)
     ;; Let the compiler know it's a special var
     (defvar vr/engine)
-    ;; Wrapper which makes isearch use visual-regexp-steroids
-    (defun vr/wrap-isearch-fun (fun &optional regexp)
+    ;; Wrapper advice which makes isearch use visual-regexp-steroids
+    (defun vr/wrap-isearch-fun (fun &rest args)
+      ;; Must be interactive, otherwise APPLY will invoke isearch in non-modal
+      ;; mode, ie. it will return immediately and thus undo our change to the
+      ;; search func.
+      (interactive)
       (let* ((isearch-search-fun-function (if (eq vr/engine 'emacs)
                                               isearch-search-fun-function
                                             #'vr--isearch-search-fun-function)))
-        ;; NOTE: We specifically take regexp arg here instead of using
-        ;; `call-interactively', since doing so will invoke isearch in
-        ;; non-modal mode, ie. it will return immediately and thus
-        ;; undo our change to the search func.
-        (funcall fun regexp)))
+        (apply fun args)))
 
-    (defun vr/isearch-backward (&optional regexp)
-      (interactive "P")
-      (vr/wrap-isearch-fun #'isearch-backward regexp))
-
-    (defun vr/isearch-forward (&optional regexp)
-      (interactive "P")
-      (vr/wrap-isearch-fun #'isearch-forward regexp))
-
-    (defun vr/isearch-backward-regexp (&optional regexp)
-      (interactive "P")
-      (vr/wrap-isearch-fun #'isearch-backward-regexp regexp))
-
-    (defun vr/isearch-forward-regexp (&optional regexp)
-      (interactive "P")
-      (vr/wrap-isearch-fun #'isearch-forward-regexp regexp))
-
-    :bind (("C-r" . vr/isearch-backward)
-           ("C-s" . vr/isearch-forward)
-           :map esc-map
-           ("C-r" . vr/isearch-backward-regexp)
-           ("C-s" . vr/isearch-forward-regexp))))
+    (advice-add #'isearch-backward        :around #'vr/wrap-isearch-fun)
+    (advice-add #'isearch-forward         :around #'vr/wrap-isearch-fun)
+    (advice-add #'isearch-backward-regexp :around #'vr/wrap-isearch-fun)
+    (advice-add #'isearch-forward-regexp  :around #'vr/wrap-isearch-fun)))
